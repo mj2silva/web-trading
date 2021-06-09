cfa31df38eeda99159c28b89209bd60d118aa084
Manuel Silva <manuel.silvag1@gmail.com>
2021-06-09 18:22:06 -0500

    Tipado de pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { FC } from 'react';
+
+import '../styles/globals.scss';
+
 import Layout from '../components/Layout/Layout';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Layout><Component {...pageProps} /></Layout>;
-}
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
+  <Layout>
+    <Component {...pageProps} />
+  </Layout>
+);
 export default MyApp;
